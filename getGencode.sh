76acf0647e570a1a8bfb2a45502cdac4c213a0b1
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2016-08-26 14:45:02 -0700

    edit

--- getGencode.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -O http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/wgEncodeGencodeBasicV19.txt.gz
-gunzip -c wgEncodeGencodeBasicV19.txt.gz > wgEncodeGencodeBasic_hg19
-echo '{ "name" : "gencode_hg19", "type" : "genePredExt", "assembly" : "hg19", "url":"http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/wgEncodeGencodeBasicV19.txt.gz"}' > wgEncodeGencodeBasic_hg19.json
