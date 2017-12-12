aa85ff954d1e21caa7fb087793a57eb8b33f850b
Jingchun Zhu <jzhu@tcga1.kilokluster.ucsc.edu>
2017-12-11 22:50:12 -0800

    update to v27

--- getGencode_hg38.sh
+++ getGencode_hg38.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -O http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/wgEncodeGencodeBasicV24.txt.gz
-gunzip -c wgEncodeGencodeBasicV24.txt.gz > wgEncodeGencodeBasic_hg38
-echo '{ "name" : "gencode_hg38", "type" : "genePredExt", "assembly" : "hg38", "url":"http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/wgEncodeGencodeBasicV24.txt.gz "}' > wgEncodeGencodeBasic_hg38.json
+curl -O http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/wgEncodeGencodeBasicV27.txt.gz
+gunzip -c wgEncodeGencodeBasicV27.txt.gz > wgEncodeGencodeBasic_hg38
+echo '{ "name" : "gencode_hg38", "type" : "genePredExt", "assembly" : "hg38", "url":"http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/wgEncodeGencodeBasicV27.txt.gz "}' > wgEncodeGencodeBasic_hg38.json
