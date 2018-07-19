62dd80d8306135ffe9da30e40122d712ca6c1501
EC2 Default User <ec2-user@ip-172-31-4-186.ec2.internal>
2018-07-19 07:06:49 +0000

    edit

--- getRefGene_mm9.sh
+++ getRefGene_mm9.sh
@@ -2,4 +2,4 @@
 
 curl -O http://hgdownload.cse.ucsc.edu/goldenPath/mm9/database/refGene.txt.gz
 gunzip -c refGene.txt.gz > refGene_mm9
-echo '{ "name" : "gencode_mm10", "type" : "genePredExt", "assembly" : "mm10", "url":"http://hgdownload.cse.ucsc.edu/goldenPath/mm9/database/refGene.txt.gz "}' > refGene_mm9.json
+echo '{ "name" : "refGene_mm9", "type" : "genePredExt", "assembly" : "mm9", "url":"http://hgdownload.cse.ucsc.edu/goldenPath/mm9/database/refGene.txt.gz "}' > refGene_mm9.json
